009eb399146deac07acdd1c70f117ecc8977bfd7
mirkoperillo <mirko.perillo@gmail.com>
2014-03-25 16:34:08 +0100

    convert object to JSON string

--- src/main/java/eu/trentorise/smartcampus/network/JsonUtils.java
+++ src/main/java/eu/trentorise/smartcampus/network/JsonUtils.java
@@ -15,10 +15,13 @@
  ******************************************************************************/
 package eu.trentorise.smartcampus.network;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig;
 import org.codehaus.jackson.map.introspect.NopAnnotationIntrospector;
@@ -30,32 +33,38 @@ import org.codehaus.jackson.type.TypeReference;
  */
 public class JsonUtils {
 
+	private static ObjectMapper fullMapper = new ObjectMapper();
+	static {
+		fullMapper.setAnnotationIntrospector(NopAnnotationIntrospector
+				.nopInstance());
+		fullMapper
+				.enable(DeserializationConfig.Feature.READ_ENUMS_USING_TO_STRING);
+		fullMapper
+				.disable(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES);
 
-    private static ObjectMapper fullMapper = new ObjectMapper();
-    static {
-        fullMapper.setAnnotationIntrospector(NopAnnotationIntrospector.nopInstance());
-        fullMapper.enable(DeserializationConfig.Feature.READ_ENUMS_USING_TO_STRING);
-        fullMapper.disable(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES);
+		fullMapper
+				.enable(SerializationConfig.Feature.WRITE_ENUMS_USING_TO_STRING);
+		fullMapper.disable(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS);
+	}
 
-        fullMapper.enable(SerializationConfig.Feature.WRITE_ENUMS_USING_TO_STRING);
-        fullMapper.disable(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS);
-    }
-    
-    /**
-     * Convert an object to object of the specified class
-     * @param object
-     * @param cls target object class
-     * @return converted object
-     */
-    public static <T> T convert(Object object, Class<T> cls) {
-    	return fullMapper.convertValue(object, cls);
-    }
+	/**
+	 * Convert an object to object of the specified class
+	 * 
+	 * @param object
+	 * @param cls
+	 *            target object class
+	 * @return converted object
+	 */
+	public static <T> T convert(Object object, Class<T> cls) {
+		return fullMapper.convertValue(object, cls);
+	}
 
-    /**
-     * Convert an object to JSON
-     * @param data
-     * @return JSON representation of the object
-     */
+	/**
+	 * Convert an object to JSON
+	 * 
+	 * @param data
+	 * @return JSON representation of the object
+	 */
 	public static String toJSON(Object data) {
 		try {
 			return fullMapper.writeValueAsString(data);
@@ -66,6 +75,7 @@ public class JsonUtils {
 
 	/**
 	 * Convert JSON String to an object of the specified class
+	 * 
 	 * @param body
 	 * @param cls
 	 * @return
@@ -79,23 +89,42 @@ public class JsonUtils {
 	}
 
 	/**
+	 * Convert object to JSON string
+	 * 
+	 */
+
+	public static String convert(Object object) {
+		try {
+			return fullMapper.writeValueAsString(object);
+		} catch (JsonGenerationException e) {
+			return null;
+		} catch (JsonMappingException e) {
+			return null;
+		} catch (IOException e) {
+			return null;
+		}
+	}
+
+	/**
 	 * Convert JSON array string to the list of objects of the specified class
+	 * 
 	 * @param body
 	 * @param cls
 	 * @return
 	 */
 	public static <T> List<T> toObjectList(String body, Class<T> cls) {
 		try {
-			List<Object> list = fullMapper.readValue(body, new TypeReference<List<?>>() { });
+			List<Object> list = fullMapper.readValue(body,
+					new TypeReference<List<?>>() {
+					});
 			List<T> result = new ArrayList<T>();
 			for (Object o : list) {
-				result.add(fullMapper.convertValue(o,cls));
+				result.add(fullMapper.convertValue(o, cls));
 			}
 			return result;
 		} catch (Exception e) {
 			return null;
 		}
 	}
-
 
 }
